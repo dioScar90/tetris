6bfa847efbf21e4088b0b26c100097db22ff2234
Diogo L Scarmagnani <diogols@live.com>
2024-01-15 23:44:00 -0300

    some styles and scripts

---
+++ assets/script.js
@@ -0,0 +1,84 @@
+const createTetrisBlocks = () => {
+  const tetris = document.querySelector('.tetris')
+  const { gridTemplateRows, gridTemplateColumns } = getComputedStyle(tetris)
+  const blocksNumber = gridTemplateRows.split(' ').length * gridTemplateColumns.split(' ').length
+
+  const fragment = document.createDocumentFragment()
+
+  for (let i = 0; i < blocksNumber; i++) {
+    const div = document.createElement('div')
+    fragment.append(div)
+  }
+
+  tetris.replaceChildren(fragment)
+}
+
+const criarTodasAsPecas = () => {
+  const criarPeca = (matrizPeca, classePeca) => {
+    const tetrisGrid = document.querySelector('.tetris')
+
+    for (let i = 0; i < matrizPeca.length; i++) {
+      for (let j = 0; j < matrizPeca[i].length; j++) {
+        if (matrizPeca[i][j] === 1) {
+          const gridItem = document.createElement('div')
+          gridItem.classList.add('grid-item', classePeca)
+          gridItem.style.gridColumn = j + 1
+          gridItem.style.gridRow = i + 1
+          tetrisGrid.append(gridItem)
+        }
+      }
+    }
+  }
+
+  // Matrizes para algumas peças Tetris comuns
+  const pecaI = [
+    [1, 1, 1, 1]
+  ]
+
+  const pecaJ = [
+    [1, 0, 0],
+    [1, 1, 1]
+  ]
+
+  const pecaL = [
+    [0, 0, 1],
+    [1, 1, 1]
+  ]
+
+  const pecaO = [
+    [1, 1],
+    [1, 1]
+  ]
+
+  const pecaS = [
+    [0, 1, 1],
+    [1, 1, 0]
+  ]
+
+  const pecaT = [
+    [0, 1, 0],
+    [1, 1, 1]
+  ]
+
+  const pecaZ = [
+    [1, 1, 0],
+    [0, 1, 1]
+  ]
+
+  // Criando peças no grid (exemplo)
+  criarPeca(pecaI, 'piece-I')
+  criarPeca(pecaJ, 'piece-J')
+  criarPeca(pecaL, 'piece-L')
+  criarPeca(pecaO, 'piece-O')
+  criarPeca(pecaS, 'piece-S')
+  criarPeca(pecaT, 'piece-T')
+  criarPeca(pecaZ, 'piece-Z')
+}
+
+
+const init = () => {
+  createTetrisBlocks()
+  // criarTodasAsPecas()
+}
+
+document.addEventListener('DOMContentLoaded', init)
